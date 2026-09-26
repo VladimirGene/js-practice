04dc91b3087675b10f5ead3e09db47a51889cf94
VladimirGene <vg135@mail.ru>
2026-09-26 10:07:38 +0500

    Внесение правок задание №7

--- script_7.js
+++ script_7.js
@@ -9,28 +9,19 @@
 // 6. Добавь в начало и конец строки символы "🔥"
 
 const str = ' javascript is awesome '
-console.log(`${str}, Длинна: ${str.length}`)
 
-const deleteSpace = text => text.trim()
-const toUpperCase = text => text.toUpperCase()
-const changeLetter = text => text.replace(/A/gi, '@')
-const slice = (text, start, end) => text.slice(start, end)
-const repeat = (text, count) => text.repeat(count)
-const changeSymbol = text => `🔥${text}🔥`
+const transformString = str => {
+	const deleteSpace = str.trim()
+	const toUpperCase = deleteSpace.toUpperCase()
+	const changeLetter = toUpperCase.replace(/A/gi, '@')
+	const sliceStr = changeLetter.slice(4, 13)
+	const repeat = (text, count) => text.repeat(count)
+	const repeatStr = repeat(`${sliceStr}-`, 3)
+	const replaceSymbol = `🔥${repeatStr}🔥`
+	return replaceSymbol
+}
 
-const trimmed = deleteSpace(str)
-const upperStr = toUpperCase(trimmed)
-const replacedLetter = changeLetter(upperStr)
-const slicedStr = slice(upperStr, 4, 13)
-const repeatStr = repeat(`${trimmed}-`, 3)
-const replaceSymbol = changeSymbol(upperStr)
-
-console.log(`1.1 ${trimmed}, Длинна: ${trimmed.length}`)
-console.log(`1.2`, upperStr)
-console.log(`1.3`, replacedLetter)
-console.log(`1.4`, slicedStr)
-console.log(`1.5`, repeatStr.slice(0, repeatStr.length - 1))
-console.log(`1.6`, replaceSymbol)
+console.log(transformString(str))
 
 // Задание 2:
 // const price = 12345.789;
@@ -42,20 +33,22 @@ console.log(`1.6`, replaceSymbol)
 
 const price = 12345.789
 
-const rounding = (price, decimal) => Number(price.toFixed(decimal))
-const roundingPrice = rounding(price, 2)
-console.log('2.1', 'Округленная цена:', roundingPrice)
+const transformPrice = price => {
+	const roundingPrice = Number(price.toFixed(2))
+	const priceThousandsSep = roundingPrice.toLocaleString()
+	const checkPriceIsInteger = Number.isInteger(roundingPrice)
+	const roundingPrice2 = Math.floor(roundingPrice * 10) / 10
 
-const priceThousandsSep = () => roundingPrice.toLocaleString()
-console.log('2.2', `${priceThousandsSep()} ₽`)
+	console.log('2.1', 'Округленная цена:', roundingPrice)
+	console.log('2.2', `${priceThousandsSep} ₽`)
+	console.log('2.3 Число не целое', checkPriceIsInteger)
+	console.log('2.4', roundingPrice2)
+	console.log('2.4', roundingPrice2.toString() + ' ₽')
+}
 
-console.log('2.3 Число не целое', Number.isInteger(roundingPrice))
-const roundingPrice2 = Math.floor(roundingPrice * 10) / 10
-console.log('2.4', roundingPrice2)
-console.log('2.4', roundingPrice2.toString() + ' ₽')
+transformPrice(price)
 
 // Задание 3.
-
 // 1. Удали пробелы с краёв
 // 2. Приведи к нижнему регистру
 // 3. Если длина меньше 4 или больше 16 символов — верни false
@@ -63,18 +56,19 @@ console.log('2.4', roundingPrice2.toString() + ' ₽')
 // 5. Если содержит пробел внутри — верни false
 // 6. Если всё ок — верни true и выведи "Логин валиден: cleanedUsername"
 function validateUsername(username) {
-	const clearValue = deleteSpace(username)
+	const clearValue = username.trim()
 	const cleanedUsername = clearValue.toLowerCase()
-	if (cleanedUsername.length < 4 || cleanedUsername.length > 16) {
+	if (
+		cleanedUsername.length < 4 ||
+		cleanedUsername.length > 16 ||
+		!Number.isNaN(Number(cleanedUsername[0])) ||
+		cleanedUsername.includes(' ')
+	) {
 		return false
-	} else if (!Number.isNaN(Number(cleanedUsername[0]))) {
-		return false
-	} else if (cleanedUsername.includes(' ')) {
-		return false
-	} else {
-		console.log(`3. Логин валиден: ${cleanedUsername}`)
-		return true
 	}
+
+	console.log(`3. Логин валиден: ${cleanedUsername}`)
+	return true
 }
 
 Тесты: validateUsername('  JohnDoe123   ') // true
@@ -84,7 +78,7 @@ validateUsername('ab') // false (слишком короткий)
 
 // Задание 4.
 const rawPhone = '  7 923 456-78-90  '
-
+const rawPhone2 = '  '
 // Сделай красивый номер +7 (923) 456-78-90
 // Используй ТОЛЬКО методы строк и конкатенацию!
 // Запрещено: split(), массивы, регулярные выражения
@@ -92,37 +86,46 @@ const rawPhone = '  7 923 456-78-90  '
 // +79234567890
 
 const updatePhone = number => {
-	const clean = number.replaceAll(' ', '').replaceAll('-', '')
-	const withPlus = `+${clean}`
-	const first = withPlus.slice(0, 2)
-	const second = withPlus.slice(2, 5)
-	const third = withPlus.slice(5, 8)
-	const fourth = withPlus.slice(8, 10)
-	const fifth = withPlus.slice(10, 12)
-	const result = `${first} (${second}) ${third}-${fourth}-${fifth}`
+	const trimmedNumber = number.trim()
+	const cleanNumber = trimmedNumber.replaceAll(' ', '').replaceAll('-', '')
+	if (cleanNumber === '') return 'Номер не указан'
+	const withPlusNumber = `+${cleanNumber}`
+	const countryCode = withPlusNumber.slice(0, 2)
+	const operatorCode = withPlusNumber.slice(2, 5)
+	const part1 = withPlusNumber.slice(5, 8)
+	const part2 = withPlusNumber.slice(8, 10)
+	const part3 = withPlusNumber.slice(10, 12)
+	const result = `${countryCode} (${operatorCode}) ${part1}-${part2}-${part3}`
 	return result
 }
 
 console.log('4.', updatePhone(rawPhone))
+console.log('4.', updatePhone(rawPhone2))
 
 // Задание 5.
 const sentence = 'Я изучаю JavaScript каждый день'
-
+const sentence2 = ''
 // Посчитай количество слов в предложении БЕЗ split() и без массивов!
 // Подсказка: перебирай строку посимвольно и считай переходы с пробела на непробел
 // Выведи: "В предложении 6 слов"
 function findLength(sentence) {
 	let count = 0
 	const cleanSentence = sentence.trim()
+	if (cleanSentence === '') {
+		console.log('5. В строке нет слов')
+		return false
+	}
 	for (let i = 0; i < cleanSentence.length; i++) {
 		if (cleanSentence[i] === ' ') {
 			count++
 		}
 	}
+
 	console.log(`5. В предложении ${count + 1} слов`)
 	return count
 }
 findLength(sentence)
+findLength(sentence2)
 
 // Задание 6.
 const fullName = ' иванов Сергей Петрович '
@@ -134,23 +137,23 @@ const fullName2 = '   петров пЕтр ВаСильевич '
 // - Инициалы от имени и отчества — заглавные с точкой
 // - Всё без массивов и split!
 
-const updateFullName = name => {
-	const clean = name.trim().toLowerCase()
+const formatNameWithInitials = name => {
+	const cleanValue = name.trim().toLowerCase()
 
-	const firstSpace = clean.indexOf(' ')
-	const secondSpace = clean.indexOf(' ', firstSpace + 1)
+	const firstSpace = cleanValue.indexOf(' ')
+	const secondSpace = cleanValue.indexOf(' ', firstSpace + 1)
 
-	const surname = clean.slice(0, firstSpace)
-	const surnameResult = surname[0].toUpperCase() + surname.slice(1)
+	const surname = cleanValue.slice(0, firstSpace)
+	const formattedSurname = surname[0].toUpperCase() + surname.slice(1)
 
-	const firstName = clean.slice(firstSpace + 1, secondSpace)
-	const firstNameResult = firstName[0].toUpperCase()
+	const firstName = cleanValue.slice(firstSpace + 1, secondSpace)
+	const firstInitial = firstName[0].toUpperCase()
 
-	const middleName = clean.slice(secondSpace + 1)
-	const middleNameResult = middleName[0].toUpperCase()
+	const middleName = cleanValue.slice(secondSpace + 1)
+	const middleInitial = middleName[0].toUpperCase()
 
-	return `${surnameResult} ${firstNameResult}. ${middleNameResult}.`
+	return `${formattedSurname} ${firstInitial}. ${middleInitial}.`
 }
 
-console.log('6.', updateFullName(fullName))
-console.log('6.', updateFullName(fullName2))
+console.log('6.', formatNameWithInitials(fullName))
+console.log('6.', formatNameWithInitials(fullName2))
